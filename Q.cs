84fda1c0e0514464843d7958da8a624ece253111
Kirill <kir.molokoff@yandex.ru>
2023-01-18 22:32:11 +0900

    finish

---
+++ Q.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine("Введите первое число: ");
+string writenum1=Console.ReadLine();
+int num1=Convert.ToInt32(writenum1);
+System.Console.WriteLine("Введите второе число: ");
+string writenum2=Console.ReadLine();
+int num2=Convert.ToInt32(writenum2);
+string txtResult= $"Число {num1} не является квадратом числа {num2}";
+if (num1==num2*num2)
+{
+    txtResult=txtResult.Replace("не"," ");
+}
+System.Console.WriteLine(txtResult);
+System.Console.WriteLine();
